8644e2eee2e5b858fab278caa5f2b35cd89ec5c5
OGWJ <owenglynwilliamjenkins@gmail.com>
2021-09-14 09:59:03 +0100

    added default case to reducer switch

--- src/reducers/searchReducer.js
+++ src/reducers/searchReducer.js
@@ -1,13 +1,15 @@
-const initState = { location: "", result: { sunrise: "", sunset: "" }, loading: false };
+const initState = { location: "London", result: { sunrise: "6", sunset: "6" }, loading: false };
 
-const searchReducer = (state=initState, action) => {
-    switch(action.type){
+const searchReducer = (state = initState, action) => {
+    switch (action.type) {
         case 'LOADING':
             return { ...state, location: action.payload, loading: true };
         case 'LOAD_RESULT':
             return { ...state, result: action.payload, loading: false, error: false };
         case 'SET_ERROR':
-            return { ...state, error: action.payload, loading: false }
+            return { ...state, error: action.payload, loading: false };
+        default:
+            return state;
     };
 };
 
