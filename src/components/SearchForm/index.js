b43d53cb2d385f9d80a336cd73f16dfb2862ad01
OGWJ <owenglynwilliamjenkins@gmail.com>
2021-09-14 10:52:44 +0100

    added preventDefault to form submit handler to prevent refresh

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -5,6 +5,7 @@ function SearchForm({ getResult }) {
     const [location, setLocation] = useState("")
 
     const handleSubmit = e => {
+        e.preventDefault();
         getResult(location);
     }
 
