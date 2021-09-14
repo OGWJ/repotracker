98ed967af4b310969a7e0681a6ad719eef58aecc
OGWJ <owenglynwilliamjenkins@gmail.com>
2021-09-14 15:54:56 +0100

    added stubs for RepoCard and CardList

---
+++ src/components/RepoCard/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RepoCard = () => {
+    return <p>Todo</p>
+}
+
+export default RepoCard;
---
+++ src/layout/CardList/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const CardList = () => {
+    return <p>CARDLIST TODO</p>
+}
