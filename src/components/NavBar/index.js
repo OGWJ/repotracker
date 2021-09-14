b5897ae9bde76da1e614e80ad8f66a449a15f048
OGWJ <owenglynwilliamjenkins@gmail.com>
2021-09-14 15:50:00 +0100

    removed redundant links from navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,21 +1,11 @@
 import React from 'react';
-import { NavLink, useHistory } from 'react-router-dom';
 import './style.css';
 
 const NavBar = () => {
 
-    const history = useHistory();
-
-    const onBackButtonClick = e => {
-        history.goBack();
-    }
-
     return (
         <nav>
-            <NavLink className="home-link" to="/">Home</NavLink>
-            <NavLink activeClassName="active" to="/about">About</NavLink>
-            <NavLink activeClassName="active" to="/search">Search</NavLink>
-            <button onClick={onBackButtonClick}>Back</button>
+            {/* this is a single page application... could put search bar in here */}
         </nav>
     );
 }
