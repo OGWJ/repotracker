d7086cb298512aad1f2d2c6b4d2cf656c2ed104c
OGWJ <owenglynwilliamjenkins@gmail.com>
2021-09-15 13:09:10 +0100

    created new empty branch for review tdue to issue with review and development branches not sharing history

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as RepoCard } from './RepoCard';
-export { default as UserSearchBar } from './UserSearchBar';
-export { default as RepoSearchBar } from './RepoSearchBar';
-export { default as CardList } from './CardList';
-export { default as Header } from './CardList';
